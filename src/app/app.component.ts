d80ae3e5591ddb003820874bc9ce7e7cb67244b2
Philip Noebauer <philip.noebauer@86400.com.au>
2024-03-22 14:02:12 +1100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule],
+})
+export class AppComponent {
+  componentTitle = "My To Do List";
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    if (!description) return;
+  
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+  
+}
